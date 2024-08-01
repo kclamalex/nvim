26fa7136d36e4ea5323a765301756257180c16be
zdate <zdatezz0528@gmail.com>
2024-08-01 16:56:38 +0100

    Adopt nvim-surround

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -18,9 +18,20 @@ local firenvim_not_active = function()
 end
 
 local plugin_specs = {
-	-- project management
+	-- Surround plugin
 	{
-		"ahmedkhalf/project.nvim",
+		"kylechui/nvim-surround",
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+			})
+		end,
+	},
+	-- proect management
+	{
+		"ahmedkhalf/proect.nvim",
 		config = function()
 			require("config.projects")
 		end,
