d475192d6b1b16e982456a3ce161e3883f0cba5a
zdate <zdatezz0528@gmail.com>
2025-06-28 23:49:08 +0100

    update treesitter

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -2,27 +2,39 @@ local treesitter = require("nvim-treesitter.configs")
 
 treesitter.setup({
 	-- A list of parser names, or "all" (the five listed parsers should always be installed)
-	ensure_installed = {
+	ensure_installed =
+	{
+		"bash",
 		"c",
-		"lua",
-		"luadoc",
-		"vim",
-		"vimdoc",
-		"query",
-		"rust",
-		"svelte",
-		"typescript",
-		"javascript",
-		"json",
 		"css",
-		"html",
-		"markdown",
-		"markdown_inline",
+		"diff",
 		"go",
 		"gomod",
 		"gowork",
 		"gosum",
-		"ruby"
+		"html",
+		"javascript",
+		"jsdoc",
+		"json",
+		"jsonc",
+		"lua",
+		"luadoc",
+		"luap",
+		"markdown",
+		"markdown_inline",
+		"printf",
+		"python",
+		"query",
+		"regex",
+		"rust",
+		"ruby",
+		"toml",
+		"tsx",
+		"typescript",
+		"vim",
+		"vimdoc",
+		"xml",
+		"yaml",
 	},
 
 	-- Install parsers synchronously (only applied to `ensure_installed`)
@@ -34,5 +46,26 @@ treesitter.setup({
 
 	highlight = {
 		enable = true,
+	},
+	indent = {
+		enable = true,
+	},
+	incremental_selection = {
+		enable = true,
+		keymaps = {
+			init_selection = "<C-space>",
+			node_incremental = "<C-space>",
+			scope_incremental = false,
+			node_decremental = "<bs>",
+		},
+	},
+	textobjects = {
+		move = {
+			enable = true,
+			goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer", ["]a"] = "@parameter.inner" },
+			goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer", ["]A"] = "@parameter.inner" },
+			goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer", ["[a"] = "@parameter.inner" },
+			goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer", ["[A"] = "@parameter.inner" },
+		},
 	},
 })
